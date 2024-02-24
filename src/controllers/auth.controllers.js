75ea8223c612eb700fbef3564e5ff833e0ad3033
Daniel Cabral <danicabral15@outlook.com>
2024-02-24 15:52:43 -0300

    agregado jwt y bcrypt a signup
    agregue encriptado de contraseña y generacion de token a la funcion signup de auth controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,6 @@
 import User from "../models/users.model.js";
+import bcrypt from "bcryptjs";
+import { createAccessToken } from "../libs/jwt.js";
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -6,15 +8,31 @@ export const signup = async (req, res) => {
   //TODO antes de crear un usuario, verificar que no exista otro con el mismo email o username.
 
   try {
+    //* Encriptar la contraseña
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    //* Crear un nuevo usuario
     const newUser = new User({
       username,
       email,
-      password,
+      password: hashedPassword,
     });
 
-    await newUser.save();
+    //* Guardar el usuario en la base de datos
+    //* el userSaved contiene los datos del usuario guardado en la base de datos
+    const userSaved = await newUser.save();
 
-    res.status(201).json({ message: "Usuario creado exitosamente" });
+    const token = await createAccessToken({ id: userSaved._id });
+
+    //* Responder al cliente con el usuario guardado
+    res.status(201).json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+      token,
+    });
   } catch (error) {
     res.status(500).json({ message: "Ocurrió un error" });
   }
