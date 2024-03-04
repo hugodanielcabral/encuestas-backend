d07a35e9335a012c41805f592da16e52ddedaad3
Daniel Cabral <danicabral15@outlook.com>
2024-03-04 14:39:10 -0300

    agregado paginacion a getEncuestas

--- src/controllers/encuestas.controller.js
+++ src/controllers/encuestas.controller.js
@@ -2,7 +2,11 @@ import Encuestas from "../models/encuestas.model.js";
 
 export const getEncuestas = async (req, res) => {
   try {
-    const encuestaData = await Encuestas.find();
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 6;
+    const skip = (page - 1) * limit; //
+
+    const encuestaData = await Encuestas.find().skip(skip).limit(limit);
 
     return res.status(200).json(encuestaData);
   } catch (error) {
