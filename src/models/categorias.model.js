e43b382870c66c8aded28fd474831f3dfe28208d
Daniel Cabral <danicabral15@outlook.com>
2024-03-07 10:57:21 -0300

    Agregado categorias.models.js

---
+++ src/models/categorias.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const categoriasSchema = new mongoose.Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    descripcion: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Categorias", categoriasSchema);
