9d97f55faf6bd843786b9d629259cda00d032890
Daniel Cabral <danicabral15@outlook.com>
2024-03-19 03:16:31 -0300

    Add CORS origin for front-end application

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
+    origin: "https://encuestas-front-t25n.onrender.com",
     credentials: true,
   })
 );
